e9b00d19394bdeef47afc2cef5b6cf1570cb27a0
hhhernan234 <hhhernan234>
2026-05-28 04:16:37 -0500

    Clase 28-05-26

--- modulo_3/blog-backend/src/mail/mail.controller.ts
+++ modulo_3/blog-backend/src/mail/mail.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { SendMailDto } from './dto/send-mail.dto';
+import { SuccessResponseDto } from '../common/dto/response.dto';
 
 @Controller('mail')
-export class MailController {}
+export class MailController {
+  constructor(private readonly mailService: MailService) {}
+
+  @Post('gmail')
+  async sendGmail(@Body() dto: SendMailDto) {
+    const result = await this.mailService.sendMail(dto);
+    return new SuccessResponseDto('Correo enviado con Gmail', result);
+  }
+}
