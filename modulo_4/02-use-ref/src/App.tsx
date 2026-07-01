b8be1f61ac14c7e1e3a0b869888acd1e41dce261
hhhernan234 <varashernan9@gmail.com>
2026-07-01 09:00:22 -0500

    Clase 01-07-2026

--- modulo_4/02-use-ref/src/App.tsx
+++ modulo_4/02-use-ref/src/App.tsx
@@ -1,9 +1,11 @@
 // src/App.tsx
 
-import AutoFocusForm    from './components/AutoFocusForm'
-import Stopwatch        from './components/Stopwatch'
-import FilterableList   from './components/FilterableList'
-import ProductAnalytics from './components/ProductAnalytics'
+/*import AutoFocusForm    from './components/AutoFocusForm'*/
+import InlineEditor from './components/InlineEditor'
+import PreviousValue from './components/PreviousValue'
+/*import Stopwatch        from './components/Stopwatch'
+/*import FilterableList   from './components/FilterableList'
+import ProductAnalytics from './components/ProductAnalytics'*/
 
 // ┌──────────────────────────────────────────────────────────────────────┐
 // │  Cambia PASO y guarda (Ctrl+S) para navegar entre componentes.      │
@@ -12,14 +14,16 @@ import ProductAnalytics from './components/ProductAnalytics'
 // │  3  FilterableList   — useCallback: función estable para hijo       │
 // │  4  ProductAnalytics — useMemo: stats y filtro memoizados           │
 // └──────────────────────────────────────────────────────────────────────┘
-const PASO = 1
+const PASO = 4
 
 export default function App() {
   const content =
     PASO === 1 ? <AutoFocusForm /> :
     PASO === 2 ? <Stopwatch /> :
-    PASO === 3 ? <FilterableList /> :
-    PASO === 4 ? <ProductAnalytics /> :
+    PASO === 3 ? <InlineEditor /> :
+    PASO === 4 ? <PreviousValue /> :
+    /*PASO === 3 ? <FilterableList /> :
+    PASO === 4 ? <ProductAnalytics /> :*/
     <p style={{ color: '#e00' }}>Paso {PASO}: crea el componente primero</p>
 
   return (
